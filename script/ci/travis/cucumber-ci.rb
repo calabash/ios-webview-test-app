ad671713a4dc83b7ac4bd287e1a43a19650bf09e
Joshua Moody <joshuajmoody@gmail.com>
2015-04-02 14:00:45 +0200

    Travis: remove redundant and unnecessary steps

--- script/ci/travis/cucumber-ci.rb
+++ script/ci/travis/cucumber-ci.rb
@@ -8,15 +8,5 @@ working_directory = File.expand_path(File.join(File.dirname(__FILE__), '..', '..
 
 # on-simulator tests of features in test/cucumber
 Dir.chdir(working_directory) do
-
-  Luffa.unix_command('bundle install',
-                     {:pass_msg => 'bundled',
-                      :fail_msg => 'could not bundle'})
-
-  # remove any stale targets
-  Luffa.unix_command('bundle exec calabash-ios sim reset',
-                     {:pass_msg => 'reset the simulator',
-                      :fail_msg => 'could not reset the simulator'})
-
   Luffa.log_info "Cucumber args: #{cucumber_args}"
 end

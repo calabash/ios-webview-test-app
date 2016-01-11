c6a447b6704146908e440c299362632c156498b4
Joshua Moody <joshuajmoody@gmail.com>
2016-01-11 15:50:20 +0100

    Travis: run cucumber in a clean bundler env
    Respond to new Gemfile and lock at root of directory

---
+++ bin/ci/travis/run.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require "bundler"
+require "luffa"
+
+working_dir = File.expand_path(File.join(".", "CalWebViewApp"))
+
+Dir.chdir(working_dir) do
+  Bundler.with_clean_env do
+
+    Luffa.unix_command("bundle install")
+    Luffa.unix_command("make app-cal")
+    Luffa.unix_command("bundle exec cucumber")
+  end
+end
+

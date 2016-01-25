b869b6c349b296ba4f489fe2b8cb6d3afe780dad
Joshua Moody <joshuajmoody@gmail.com>
2016-01-25 07:52:20 +0100

    Cucumber: start each scenario at the top of the page

--- CalWebViewApp/features/steps/tabbar_steps.rb
+++ CalWebViewApp/features/steps/tabbar_steps.rb
@@ -6,4 +6,8 @@ Given(/^I am looking at the (UIWebView|WKWebView) tab$/) do |tab_name|
   else
     tabbar.navigate_to(:wkwebview)
   end
+
+  js = "window.scrollTo(0,0)"
+  query(tab_name, {calabashStringByEvaluatingJavaScript:js})
+  wait_for_none_animating
 end

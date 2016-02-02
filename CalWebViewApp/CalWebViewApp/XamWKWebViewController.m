47f93ef158324012d55fb0cbacfa5f6161e65865
Joshua Moody <joshuajmoody@gmail.com>
2016-02-02 17:24:23 +0100

    WKWebView: host page.html on cdn.rawgit.com
    Hosting from githubusercontent just displays raw html

--- CalWebViewApp/CalWebViewApp/XamWKWebViewController.m
+++ CalWebViewApp/CalWebViewApp/XamWKWebViewController.m
@@ -235,7 +235,7 @@ typedef enum : NSUInteger {
                                                      ofType:@"html"];
     NSURL *url = [NSURL fileURLWithPath:path];
 #else
-    NSString *page = @"https://raw.githubusercontent.com/calabash/ios-webview-test-app/master/CalWebViewApp/CalWebViewApp/page.html";
+    NSString *page = @"https://cdn.rawgit.com/calabash/ios-webview-test-app/master/CalWebViewApp/CalWebViewApp/page.html";
     NSURL *url = [NSURL URLWithString:page];
 #endif
 
